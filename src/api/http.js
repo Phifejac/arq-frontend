68088b01fc054d100f379cc49d9957c5cd68cf87
Jack Phifer <jphifer@mit.edu>
2020-12-29 21:21:11 -0500

    added authentication

--- src/api/http.js
+++ src/api/http.js
@@ -11,9 +11,10 @@ export const login = async (username, password) => {
     }
     const res = await axios.post(`/api/login`, body );
     if (res.data.jwt) {
-      sessionStorage.setItem("jwt", res.data.jwt)
-      sessionStorage.setItem("username", username)
-      const jwt = await sessionStorage.getItem("jwt")
+      console.log("SET", res.data.jwt)
+      localStorage.setItem("jwt", res.data.jwt)
+      localStorage.setItem("username", username)
+      const jwt = await localStorage.getItem("jwt")
     }
     return res.data;
   } catch (err) {
@@ -39,152 +40,236 @@ export const register = async (username, email, password) => {
 
 // get all transactions subject to a query string
 export const getTransactions = async (parameters) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
     const queryString = querystring.parse()
     for (const param in parameters) {
       queryString[param] = parameters[param]
     }
-    const res = await axios.get(`/api/transaction?` + querystring.stringify(queryString));
+    const res = await axios.get(`/api/transaction?` + querystring.stringify(queryString), {headers : headers});
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // create a new transaction 
 export const createTransaction = async (transaction_fields) => {
+  const headers = {
+    "Authorization" : "Bearer " + await + localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.post(`/api/transaction`, transaction_fields);
+    const res = await axios.post(`/api/transaction`, transaction_fields, {headers : headers});
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // update transaction 
 export const updateTransaction = async (transaction_fields) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.put(`/api/transaction`, transaction_fields);
+    const res = await axios.put(`/api/transaction`, transaction_fields, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // delete transaction 
 export const deleteTransaction = async (transaction_fields) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.delete(`/api/transaction`, {params: transaction_fields});
+    const res = await axios.delete(`/api/transaction`, {params: transaction_fields, headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 export const getHomeStatistics = async () => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.get(`/api/statistic/overview`);
+    const res = await axios.get(`/api/statistic/overview`, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 
 export const getLogs = async () => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.get(`/api/history`);
+    const res = await axios.get(`/api/history`, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 export const getClients = async () => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.get(`/api/clients`);
+    const res = await axios.get(`/api/clients`, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // delete client
 export const deleteClient = async (info) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.delete(`/api/clients`, {params: info});
+    const res = await axios.delete(`/api/clients`, { params: info, headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // update client
 export const updateClient = async (info) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.put(`/api/clients`, info);
+    const res = await axios.put(`/api/clients`, info, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 // add client
 export const addClient = async (info) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.post(`/api/clients`, info);
+    const res = await axios.post(`/api/clients`, info, { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 
 export const getOpenPositions = async () => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   try {
-    const res = await axios.get("/api/transaction?min_open_qty=0.000000001&owner")
+    const res = await axios.get("/api/transaction?min_open_qty=0.000000001&owner", { headers : headers })
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 export const getInstances = async (parameters) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   const queryString = querystring.parse()
   for (const param in parameters) {
     queryString[param] = parameters[param]
   }
   try {
-    const res = await axios.get("/api/instance?" + querystring.stringify(queryString))
+    const res = await axios.get("/api/instance?" + querystring.stringify(queryString), { headers : headers })
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 export const getStatistics= async (parameters) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   const queryString = querystring.parse()
   for (const param in parameters) {
     queryString[param] = parameters[param]
   }
   try {
-    const res = await axios.get("/api/statistic?" + querystring.stringify(queryString));
+    const res = await axios.get("/api/statistic?" + querystring.stringify(queryString), { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 }
 
 
 export const getStatisticsRange = async (parameters) => {
+  const headers = {
+    "Authorization" : "Bearer " + await localStorage.getItem("jwt")
+  }
   const queryString = querystring.parse()
   for (const param in parameters) {
     queryString[param] = parameters[param]
   }
   try {
-    const res = await axios.get("/api/statistic/range?" + querystring.stringify(queryString));
+    const res = await axios.get("/api/statistic/range?" + querystring.stringify(queryString), { headers : headers });
     return res.data;
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = "/auth/login"
+    }
     throw err;
   }
 } 
